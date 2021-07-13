7ae8800348a713e80d30e9b4dc7c9f5404bd5e96
Christian Lobarede <ch.lobarede@gmail.com>
2021-07-13 18:49:33 -0400

    Mi primer Componente

---
+++ src/components/NuevoComponente.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class NuevoComponente extends Component{
+    render(){
+        return(
+            <div>Hola Mundo y Galaxia !</div>
+        );
+    }
+}
+
+export default NuevoComponente;
